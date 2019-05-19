491e8e6177844dfe18af92e03afdfa5869daf67f
Md. Atiquzzaman Soikat <atik.aiub@gmail.com>
2019-05-19 14:14:21 +0600

    enabled cors

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 var express = require('express');
 var jsonServer = require('json-server');
-
+var cors = require('cors');
 var server = express();
 // ...
 
 // You may want to mount JSON Server on a specific end-point, for example /api
 // Optiona,l except if you want to have JSON Server defaults
 // server.use('/api', jsonServer.defaults()); 
+
+server.use(cors());
 server.use('/api', jsonServer.router('./db/db.json'));
 
 server.listen(4000, function(){
