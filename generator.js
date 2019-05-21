43000cff6ffab37f206529959604bd8bd4dede43
Md. Atiquzzaman Soikat <atik.aiub@gmail.com>
2019-05-21 12:44:08 +0600

    updated generator.js

--- generator.js
+++ generator.js
@@ -8,7 +8,12 @@ module.exports = function (){
 				id: n,
 				companyName: faker.company.companyName(),
 				email: faker.internet.email(),
-				address: faker.address.streetAddress("###")	
+				address: faker.address.streetAddress("###"),
+		        mainBgColor: faker.internet.color(),
+  				headerBgColor: faker.internet.color(),
+                headerTextColor: faker.internet.color(),
+                headerImage: faker.image.imageUrl(),
+                footerImage: faker.image.imageUrl()
 			}
 		})
 	}
